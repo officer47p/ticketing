ff280a08d2a5a85c27be609875e3d6311a6a442c
Parsa Hosseini <officer47p@gmail.com>
2022-07-22 13:44:06 +0430

    Create validate-request middleware to prevent code duplication

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+
+import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/api/users/signin", async (req, res) => {
-  res.send("Hello signin route");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  }
+);
 
 export { router as signinRouter };
