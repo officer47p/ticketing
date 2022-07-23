302b29260d85feec3d0b0db486a86e45d03689e9
Parsa Hosseini <officer47p@gmail.com>
2022-07-23 13:31:38 +0430

    Add not authenticated route tests

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,13 +4,8 @@ import { currentUser } from "../middlewares/current-user";
 
 const router = Router();
 
-router.get(
-  "/api/users/currentuser",
-  currentUser,
-
-  async (req, res) => {
-    res.send({ currentUser: req.currentUser || null });
-  }
-);
+router.get("/api/users/currentuser", currentUser, async (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+});
 
 export { router as currentUserRouter };
