7b1a57feb27ef21ada71f122fccb66ebdb0be36a
Parsa Hosseini <officer47p@gmail.com>
2022-08-06 14:51:13 +0430

    Add ticket model and add tests for creating ticket

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets/ for post request", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -22,8 +23,66 @@ it("returns an status other than 401 when the user is signed in", async () => {
   expect(response.statusCode).not.toEqual(401);
 });
 
-it("returns an error if an invalid title is provided", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+  const response1 = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send({
+      title: "",
+      price: 10,
+    });
 
-it("returns an error if an invalid price is provided", async () => {});
+  expect(response1.statusCode).toEqual(400);
 
-it("creates a ticket with valid inputs", async () => {});
+  const response2 = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send({
+      price: 10,
+    });
+
+  expect(response2.statusCode).toEqual(400);
+});
+
+it("returns an error if an invalid price is provided", async () => {
+  const response1 = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send({
+      title: "dsjnfdsk",
+      price: -10,
+    });
+
+  expect(response1.statusCode).toEqual(400);
+
+  const response2 = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send({
+      title: "dsjnfdsk",
+    });
+
+  expect(response2.statusCode).toEqual(400);
+});
+
+it("creates a ticket with valid inputs", async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets).toHaveLength(0);
+
+  const ticketPayload = {
+    title: "kjdnfkds",
+    price: 10,
+  };
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send(ticketPayload);
+
+  expect(response.statusCode).toEqual(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets).toHaveLength(1);
+  expect(tickets[0].title).toEqual(ticketPayload.title);
+  expect(tickets[0].price).toEqual(ticketPayload.price);
+});
