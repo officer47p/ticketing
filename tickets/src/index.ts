dd6927ce1972977171baff85b38d7ada2ae396b4
Parsa Hosseini <officer47p@gmail.com>
2022-08-08 16:46:47 +0430

    Add tickets service to the ingress configuration

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -15,8 +15,8 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
-  app.listen(3002, () => {
-    console.log("Listening on port 3002");
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
   });
 };
 
